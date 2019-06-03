334092d5b05cd7df30adc23bfb222580f616dabd
Brad Campbell <bradjc5@gmail.com>
2019-06-03 19:56:29 -0400

    simplify blink

--- examples/blink/main.c
+++ examples/blink/main.c
@@ -5,18 +5,22 @@ int main(void) {
   // Ask the kernel how many LEDs are on this board.
   int num_leds = led_count();
 
-  // Blink the LEDs in a binary count pattern and scale
-  // to the number of LEDs on the board.
-  for (int count = 0; ; count++) {
-    for (int i = 0; i < num_leds; i++) {
-      if (count & (1 << i)) {
-        led_on(i);
-      } else {
-        led_off(i);
-      }
-    }
+  led_on(0);
+  led_off(1);
+  led_on(2);
 
-    // This delay uses an underlying timer in the kernel.
-    delay_ms(250);
-  }
+  // // Blink the LEDs in a binary count pattern and scale
+  // // to the number of LEDs on the board.
+  // for (int count = 0; ; count++) {
+  //   for (int i = 0; i < num_leds; i++) {
+  //     if (count & (1 << i)) {
+  //       led_on(i);
+  //     } else {
+  //       led_off(i);
+  //     }
+  //   }
+
+  //   // This delay uses an underlying timer in the kernel.
+  //   delay_ms(250);
+  // }
 }
